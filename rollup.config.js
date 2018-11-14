6115bf31f99c679e54134a22ff7c50d9c489780d
Ilya Semenov <semenov@inetss.com>
2018-11-14 21:49:17 +0700

    formatting

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,15 @@ import cleaner from 'rollup-plugin-cleaner'
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/bundle.umd.js',
-    format: 'umd',
-    name: 'dataCleaner',
-    sourcemap: true,
-  },
-  plugins: [
+	input: 'src/index.ts',
+	output: {
+		file: 'dist/bundle.umd.js',
+		format: 'umd',
+		name: 'dataCleaner',
+		sourcemap: true,
+	},
+	plugins: [
 		cleaner({ targets: ['./dist/'] }),
-    typescript(),
-  ]
+		typescript(),
+	]
 }
