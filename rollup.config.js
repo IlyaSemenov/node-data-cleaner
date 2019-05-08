83904f3dd67fdd3d77747fb0b0e2759ff5027546
Ilya Semenov <semenov@inetss.com>
2019-05-08 23:05:42 +0700

    chore: rollup as CJS instead of UMD
    BREAKING CHANGE: Use CJS instead of UMD as rollup bundle type

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,8 @@ import typescript from 'rollup-plugin-typescript2'
 export default {
 	input: 'src/index.ts',
 	output: {
-		file: 'dist/bundle.umd.js',
-		format: 'umd',
-		name: 'dataCleaner',
+		file: 'dist/index.js',
+		format: 'cjs',
 		sourcemap: true,
 	},
 	plugins: [cleaner({ targets: ['./dist/'] }), typescript()],
